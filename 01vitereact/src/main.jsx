70638325cfdcdbd28468f4389f5cadc12072c24a
Ronak Khandelwal <52578561+ronakkhandlwl@users.noreply.github.com>
2023-11-25 08:47:43 +0000

    Understand inner working of jsx

---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://google.com",
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const ReactElement = React.createElement(
+  'a',
+  {href: "https://google.com",
+   target: "_blank"},
+  'Click me to visit Google'
+)
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Click me to visit google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    ReactElement
+)
